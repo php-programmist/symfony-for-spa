a99f09e8fa24efc9a63ac9aabd5cfb0761a4d58a
Алексей Костин <php.programmist.ru@gmail.com>
2021-03-22 14:50:54 +0300

    Добавил блокировку входа из-за последовательных неудачных попыток войти

--- src/EventSubscriber/LoginSubscriber.php
+++ src/EventSubscriber/LoginSubscriber.php
@@ -8,6 +8,7 @@ use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Lexik\Bundle\JWTAuthenticationBundle\Events;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Throwable;
 
@@ -27,7 +28,10 @@ class LoginSubscriber implements EventSubscriberInterface
     {
         return [
             Events::AUTHENTICATION_SUCCESS => 'onAuthenticationSuccess',
-            Events::AUTHENTICATION_FAILURE => 'onAuthenticationFailureEvent'
+            Events::AUTHENTICATION_FAILURE => 'onAuthenticationFailureEvent',
+            RequestEvent::class => [
+                ['onKernelRequest', 9]
+            ]
         ];
     }
 
@@ -49,5 +53,19 @@ class LoginSubscriber implements EventSubscriberInterface
         /** @var User $user */
         $user = $event->getUser();
         $this->userManager->setAuthenticationSuccess($user);
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+        $request = $event->getRequest();
+        if ($request->attributes->get('_route') === 'authentication_token') {
+            $response = $this->userManager->getLoginBlockResponse($event->getRequest()->getContent());
+            if (null !== $response) {
+                $event->setResponse($response);
+            }
+        }
     }
 }
--- src/Service/UserManager.php
+++ src/Service/UserManager.php
@@ -6,7 +6,10 @@ namespace App\Service;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use JsonException;
 use Redis;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserManager
 {
@@ -15,6 +18,7 @@ class UserManager
 
     /**
      * @param EntityManagerInterface $entityManager
+     * @param Redis $redis
      */
     public function __construct(EntityManagerInterface $entityManager, Redis $redis)
     {
@@ -39,12 +43,38 @@ class UserManager
 
         $delay = $user->getLoginBlockDelay();
         if ($delay > 0) {
-            $this->redis->setex($this->getLoginBlockKey($email), $delay, true);
+            $this->redis->setex($this->getLoginBlockKey($email), $delay, $user->getFailedLoginCount());
         }
     }
 
     public function getLoginBlockKey(string $email): string
     {
         return 'login:block:' . $email;
+    }
+
+    /**
+     * @param string $requestBody
+     * @return Response|null
+     * @throws JsonException
+     */
+    public function getLoginBlockResponse(string $requestBody): ?Response
+    {
+        $response = null;
+        $data = json_decode($requestBody, true, 512, JSON_THROW_ON_ERROR);
+        $email = $data['email'];
+        $code = Response::HTTP_BAD_REQUEST;
+
+        $key = $this->getLoginBlockKey($email);
+        $failedCount = $this->redis->get($key);
+        if ($failedCount > 0) {
+            $ttl = $this->redis->ttl($key);
+            $response = new JsonResponse([
+                'code' => $code,
+                'message' => sprintf('Аккаунт заблокирован на %d секунд из-за %d последовательных неудачных попыток входа в систему',
+                    $ttl, $failedCount),
+            ], $code);
+        }
+
+        return $response;
     }
 }
