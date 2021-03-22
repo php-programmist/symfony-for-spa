c689697c3a02cdb800db0418bf5ded3980c16c7b
Алексей Костин <php.programmist.ru@gmail.com>
2021-03-22 10:20:23 +0300

    Добавил поля для хранения данных о попытках аутентификации

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,12 +3,12 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="`user`")
  */
 class User implements UserInterface
 {
@@ -17,23 +17,38 @@ class User implements UserInterface
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private ?int $id = null;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      */
-    private $email;
+    private ?string $email = null;
 
     /**
      * @ORM\Column(type="json")
      */
-    private $roles = [];
+    private array $roles = [];
 
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
      */
-    private $password;
+    private string $password;
+
+    /**
+     * @ORM\Column(type="integer", options={"default": 0})
+     */
+    private int $failedLoginCount = 0;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private ?DateTime $lastLoginAttempt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private ?DateTime $lastLogin;
 
     public function getId(): ?int
     {
@@ -114,5 +129,57 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFailedLoginCount(): int
+    {
+        return $this->failedLoginCount;
+    }
+
+    /**
+     * @param int $failedLoginCount
+     * @return $this
+     */
+    public function setFailedLoginCount(int $failedLoginCount): self
+    {
+        $this->failedLoginCount = $failedLoginCount;
+        return $this;
+    }
+
+    /**
+     * @return DateTime|null
+     */
+    public function getLastLoginAttempt(): ?DateTime
+    {
+        return $this->lastLoginAttempt;
+    }
+
+    /**
+     * @param DateTime|null $lastLoginAttempt
+     */
+    public function setLastLoginAttempt(?DateTime $lastLoginAttempt): void
+    {
+        $this->lastLoginAttempt = $lastLoginAttempt;
+    }
+
+    /**
+     * @return DateTime|null
+     */
+    public function getLastLogin(): ?DateTime
+    {
+        return $this->lastLogin;
+    }
+
+    /**
+     * @param DateTime|null $lastLogin
+     * @return $this
+     */
+    public function setLastLogin(?DateTime $lastLogin): self
+    {
+        $this->lastLogin = $lastLogin;
+        return $this;
     }
 }
